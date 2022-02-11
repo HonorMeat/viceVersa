386c2d1956c96dd0e3bb0840f13f84c9af6e67d6
HonorMeat <fig010104@mail.ru>
2022-02-12 00:05:48 +0600

    New LOGIC

--- viceVersa/views.py
+++ viceVersa/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {'greeting':'Hello!'})
+def reverse(request):
+    reverse_me = request.GET['reverse_me']
+    reversed = reverse_me[::-1]
+    return render(request, 'reverse.html', {'reversed': reversed, 'reverse_me': reverse_me})
